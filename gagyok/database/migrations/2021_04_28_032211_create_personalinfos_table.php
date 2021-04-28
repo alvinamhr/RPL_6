bcee97d5980b1a0fe1d50790c3a4894c29f614c9
Alifikrii <andimuhammadalifikri@apps.ipb.ac.id>
2021-04-28 12:27:20 +0800

    menambahkan file migrate database pada database/migrations

---
+++ gagyok/database/migrations/2021_04_28_032211_create_personalinfos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalinfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personalinfos', function (Blueprint $table) {
+            $table->bigInteger('user_id')->references('id')->on('users');
+            $table->string('phone_number');
+            $table->string('user_email')->references('email')->on('users');
+            $table->string('user_address');
+            $table->string('user_city');
+            $table->string('user_disctrict');
+            $table->string('user_province');
+            $table->string('user_posCode');
+            $table->string('user_country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personalinfos');
+    }
+}
---
+++ gagyok/database/migrations/2021_04_28_040935_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('product_id')->unique();
+            $table->char('product_name',100);
+            $table->string('product_image');
+            $table->double('product_price', 11, 2);
+            $table->string('product_stock');
+            $table->string('product_desc',);
+            $table->string('product_category',);
+            $table->double('product_discount',11, 2);
+            $table->foreign('product_category')->references('category_name')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
